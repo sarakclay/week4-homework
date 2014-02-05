1054cdc4708a4f8166703862bdcf8cb06150f9ca
Sara Clay <sarakclay@gmail.com>
2014-02-05 13:03:21 -0600

    Week 4 homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Week4hw::Application.routes.draw do
 
-  get "/" => "pages#home"
-
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
   # HINT: Add more routes here...
 
 
